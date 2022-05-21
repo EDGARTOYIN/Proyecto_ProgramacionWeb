291b9c39b4be16861328630474a331bd39b5a032
Edgar Rebollo <edgar-toyin1@hotmail.com>
2022-05-20 18:25:21 -0700

    initial setup

---
+++ js/script_countD.js
@@ -0,0 +1,29 @@
+const countdown = () =>{
+
+const countDate = new Date("May 27,2022 00:00:00").getTime();
+const now = new Date().getTime();
+const gap = countDate-now;
+
+
+const seconds = 1000;
+const minutes = seconds*60;
+const hour = minutes*60;
+const day = hour *24
+
+
+const textDay = Math.floor(gap /day);
+const textHour = Math.floor((gap%day) /hour);
+const textMinute = Math.floor((gap%hour) /minutes);
+const textSecond = Math.floor((gap%minutes) /seconds);
+
+document.querySelector(".day").innerHTML=textDay;
+document.querySelector(".hour").innerText =textHour;
+document.querySelector(".minutes").innerText =textMinute;
+document.querySelector(".seconds").innerText =textSecond;
+
+
+
+};
+
+
+setInterval(countdown, 1000);
